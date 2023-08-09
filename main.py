0efdfa8532da1916545f686c8f26f598ca9c2f79
Aniket Yadav <83213012+purethunder110@users.noreply.github.com>
2023-08-09 20:01:40 +0530

    args moment

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from wsgi import app
-import subprocess
+import webview
+import sys
+import threading
+import argparse
 
-#subprocess.Popen("waitress-serve --host 127.0.0.1 --call wsgi:app",shell=True)
+#argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("--type",required=True,help="type of function that you want to execute. web for website and app for application")
 
-app.run(debug=True)
+def start_server():
+    app.run(host='0.0.0.0',port=5000)
+
+if __name__ == '__main__':
+    args=parser.parse_args()
+    if args.type=="web":
+        start_server()
+    elif args.type=="app":
+        t = threading.Thread(target=start_server)
+        t.daemon = True
+        t.start()
+        webview.create_window("yt-remote-control","http://localhost:5000/",confirm_close=True,)
+        webview.start()
+        sys.exit()
+    else:
+        raise "--type argument is wrong. it could either be a 'web' type or a 'app' type"
